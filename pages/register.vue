9c957d8a8a86690016d0d81156a3318af443795c
ruffi0 <matthew@motorcity.host>
2023-06-11 23:59:20 -0400

    add custom url

--- pages/register.vue
+++ pages/register.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="max-w-md mx-auto mt-8">
-      <h1 class="text-2xl font-bold mb-4">Register</h1>
-      <form @submit.prevent="sendInvitation">
-        <div class="mb-4">
-          <label for="email" class="block mb-1">Email</label>
-          <input v-model="email" type="email" id="email" required />
-        </div>
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Send Invitation</button>
-      </form>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref } from 'vue'
-  const { $directus } = useNuxtApp()
-  
-  const email = ref('')
-  
-  async function sendInvitation() {
-    try {
-      await $directus.users.invites.send(email.value, '0bd42dcf-4815-4be1-8319-113c3dd77eba')
-      console.log('Invitation sent')
-      // Optionally, you can redirect the user to a different page or show a success message.
-    } catch (error) {
-      console.error('Error sending invitation:', error)
-      // Handle the error, e.g., show an error message to the user.
-    }
+  <div class="max-w-md mx-auto mt-8">
+    <h1 class="text-2xl font-bold mb-4">Register</h1>
+    <form @submit.prevent="sendInvitation">
+      <div class="mb-4">
+        <label for="email" class="block mb-1">Email</label>
+        <input v-model="email" type="email" id="email" required />
+      </div>
+      <input type="hidden" name="invite_url" :value="inviteUrl" />
+      <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Send Invitation</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const { $directus } = useNuxtApp()
+
+const email = ref('')
+
+const inviteUrl = `${window.location.origin}/accept-invite`
+
+async function sendInvitation() {
+  try {
+    await $directus.users.invites.send(email.value, '0bd42dcf-4815-4be1-8319-113c3dd77eba')
+    console.log('Invitation sent')
+    // Optionally, you can redirect the user to a different page or show a success message.
+  } catch (error) {
+    console.error('Error sending invitation:', error)
+    // Handle the error, e.g., show an error message to the user.
   }
-  </script>
-  
+}
+</script>
