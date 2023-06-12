3061ff7936c7bd038fa54df5f51ffa4b5a121acd
ruffi0 <matthew@motorcity.host>
2023-06-11 20:21:32 -0400

    test commit

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -9,22 +9,25 @@
     </NuxtLink>
 
     <div class="relative pt-48 pb-10 overflow-hidden shadow-xl rounded-2xl">
+      <ClientOnly fallback-tag="span" fallback="Loading image...">
       <img
         class="absolute inset-0 object-cover w-full h-full"
         :src="fileUrl(page.image)"
       />
-      <div class="absolute inset-0 bg-primary-500 mix-blend-multiply" />
-      <div
-        class="absolute inset-0 bg-gradient-to-t from-primary-600 via-primary-600 opacity-80"
-      />
+      </ClientOnly>
+      <div class="absolute inset-0 bg-primary-500 mix-blend-multiply">
+      <div class="absolute inset-0 bg-gradient-to-t from-primary-600 via-primary-600 opacity-80">
       <div class="relative px-8">
         <div class="relative text-lg font-medium text-white md:flex-grow">
           <h1 class="text-6xl font-bold drop-shadow-sm">{{ page.title }}</h1>
         </div>
       </div>
     </div>
-    <div class="prose dark:prose-invert" v-html="page.content" />
+    <div class="prose dark:prose-invert" v-html="page.content">
   </div>
+  </div>
+    </div>
+</div>
 </template>
 
 <script setup>
