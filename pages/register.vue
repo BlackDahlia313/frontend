d64642e773787a5f8deb2d601d5d5ddb18aa308e
ruffi0 <matthew@motorcity.host>
2023-06-12 00:33:24 -0400

    heroicons

--- pages/register.vue
+++ pages/register.vue
@@ -12,7 +12,9 @@
           <ListboxButton class="relative w-full py-2 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-sm cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
             <span class="block truncate">{{ selectedRole ? selectedRole.name : 'Select a role' }}</span>
             <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true" />
+              <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+              </svg>
             </span>
           </ListboxButton>
           <ListboxOptions class="absolute z-10 w-full py-1 mt-1 overflow-auto bg-white border border-gray-300 rounded-md shadow-lg max-h-60 sm:text-sm" v-slot="{ active }">
@@ -36,7 +38,7 @@
 <script setup>
 import { ref } from 'vue'
 import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
-import { SelectorIcon } from '@heroicons/vue/solid'
+const { SelectorIcon } = require('@heroicons/vue/24/solid') // Import the SelectorIcon from the Heroicons Vue package
 const { $directus } = useNuxtApp()
 
 const email = ref('')
