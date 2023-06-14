ccd24948cf77c663f2946817dd577cde910a551f
ruffi0 <matthew@motorcity.host>
2023-06-14 00:44:43 -0400

    should be it

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,18 @@
-import { storeToRefs } from 'pinia'
-// import { onMounted } from 'vue'
 import { useAuth } from '~/store/auth'
+import { onMounted } from 'vue'
 
-const msg =
-  'You must be logged in to view this page.'
+const msg = 'You must be logged in to view this page.'
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(() => {
   const user = useAuth()
-  // Get user data from the store
-const { isLoggedIn } = storeToRefs(auth)
 
-// // Fetch user data on component mount
-
-//   auth.getUser()
-
-
-  if (!user.isLoggedIn) {
-    if (process.client) {
-      window.alert(msg)
+  onMounted(() => {
+    if (!user.isLoggedIn) {
+      if (process.client) {
+        window.alert(msg)
+      }
+      console.log(msg)
+      return navigateTo('/')
     }
-    console.log(msg)
-
-    return navigateTo('/')
-  }
+  })
 })
