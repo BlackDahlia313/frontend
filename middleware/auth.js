e9488ea3e82f6fc915d5ef29f9e2072bf43ea479
ruffi0 <matthew@motorcity.host>
2023-06-14 01:11:23 -0400

    saf

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,17 @@
 import { useAuth } from '~/store/auth'
 
-const msg = 'You must be logged in to view this page.'
+const msg = 'You must be logged in to view this page. Redirecting back to the home page.'
 
-export default function ({ store, redirect, from }) {
+export default defineNuxtRouteMiddleware(({ redirect }) => {
   const user = useAuth()
 
-  // Check if the user is logged out
-  if (!user.isLoggedIn) {
-    // Check if the previous route is not the current route
-    // This check is to avoid alert and redirection when accessing the page directly via the URL
-    if (from?.path !== null && from?.path !== '/') {
+  onMounted(() => {
+    if (!user.isLoggedIn) {
       if (process.client) {
         window.alert(msg)
       }
       console.log(msg)
-      return navigateTo('/')
+      navigateTo('/')
     }
-  }
-}
+  })
+})
