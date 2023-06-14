33d3931d2782cf63c4e4aaba6942dc87423760da
ruffi0 <matthew@motorcity.host>
2023-06-14 00:31:47 -0400

    middleware upgrade test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,19 @@
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
 import { useAuth } from '~/store/auth'
+
 const msg =
   'You must be logged in to view this page.'
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const user = useAuth()
+  const auth = useAuth()
+  // Get user data from the store
+const { isLoggedIn, user } = storeToRefs(auth)
+
+// Fetch user data on component mount
+onMounted(() => {
+  auth.getUser()
+})
 
   if (!user.isLoggedIn) {
     if (process.client) {
