04a8df2561da9ac5882c618914cf2aa268853f91
ruffi0 <matthew@motorcity.host>
2023-06-13 23:42:26 -0400

    change for onounted

--- components/Examples/Auth.vue
+++ components/Examples/Auth.vue
@@ -9,6 +9,7 @@
 
       <div class="not-prose">
         <NuxtLink
+          v-if="isLoggedIn"
           class="inline-flex items-center font-bold text-primary-600 group hover:text-primary-800 dark:text-primary-200 dark:hover:text-primary-400"
           to="/protected-page"
           >View Protected Content <span class="ml-2 text-xl">→</span></NuxtLink
@@ -19,7 +20,7 @@
       <div class="space-y-12">
         <!-- Login Form -->
         <div class="">
-          <ExamplesLoginForm v-if="!isLoggedIn" />
+          <ExamplesLoginForm v-if="!isLoggedIn" @login="login" />
           <div v-if="isLoggedIn" class="flex items-center justify-between">
             <div class="flex items-center">
               <img
@@ -35,11 +36,12 @@
                 </p>
               </div>
             </div>
-            <VButton variant="primary" @click="auth.logout()"> Logout</VButton>
+            <VButton variant="primary" @click="logout">Logout</VButton>
           </div>
         </div>
         <!-- User Details -->
         <div
+          v-if="isLoggedIn"
           class="flex flex-col p-4 rounded-lg bg-gradient-to-b from-primary-600 to-primary-900"
         >
           <p
@@ -52,7 +54,7 @@
           </p>
           <pre
             class="h-full p-4 mt-4 overflow-x-auto text-sm text-gray-100 whitespace-pre bg-gray-800 border-2 rounded-lg border-primary-800"
-            >{{ user }}</pre
+          >{{ user }}</pre
           >
         </div>
       </div>
@@ -60,11 +62,71 @@
   </ExamplesTwoCols>
 </template>
 
-<script setup>
-// Get user data from the store
-import { storeToRefs } from 'pinia'
+<script>
+import { ref, onMounted } from 'vue'
+import { useNuxtApp, useFiles } from 'nuxt-composition-api'
 import { useAuth } from '~~/store/auth'
-const auth = useAuth()
-const { fileUrl } = useFiles()
-const { isLoggedIn, user } = storeToRefs(auth)
+
+export default {
+  setup() {
+    const { $directus } = useNuxtApp()
+    const { fileUrl } = useFiles()
+    const auth = useAuth()
+    const isLoggedIn = ref(auth.isLoggedIn)
+    const user = ref(auth.userData)
+
+    // Login action
+    const login = async ({ email, password }) => {
+      try {
+        // Try to login
+        const response = await $directus.auth.login({
+          email,
+          password,
+        })
+
+        // Fetch the user's data
+        await auth.getUser()
+
+        // Update the local refs
+        isLoggedIn.value = auth.isLoggedIn
+        user.value = auth.userData
+      } catch (e) {
+        console.log(e)
+        throw new Error('Wrong email address or password')
+      }
+    }
+
+    // Logout action
+    const logout = async () => {
+      try {
+        // Try to logout
+        await auth.logout()
+
+        // Update the local refs
+        isLoggedIn.value = auth.isLoggedIn
+        user.value = auth.userData
+      } catch (e) {
+        console.log(e)
+        throw new Error('Issue logging out')
+      }
+    }
+
+    // Fetch user data on component mount
+    onMounted(async () => {
+      await auth.getUser()
+
+      // Update the local refs
+      isLoggedIn.value = auth.isLoggedIn
+      user.value = auth.userData
+    })
+
+    return {
+      fileUrl,
+      isLoggedIn,
+      user,
+      login,
+      logout,
+    }
+  },
+}
 </script>
