991b7d0364d2f91e0491079c1b8a1d8d498c1984
ruffi0 <matthew@motorcity.host>
2023-06-14 01:35:12 -0400

    sdf

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,19 @@ import { useAuth } from '~/store/auth'
 const msg =
   'You must be logged in to view this page. Redirecting back to home page.'
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware({
+  cache: {
+    ttl: 600000, // 10 minutes
+  },
+})((to, from) => {
   const user = useAuth()
 
-  user.watch((state) => {
-    if (!state.isLoggedIn) {
-      if (process.client) {
-        window.alert(msg)
-      }
-      console.log(msg)
-
-      return navigateTo('/')
+  if (!user.isLoggedIn) {
+    if (process.client) {
+      window.alert(msg)
     }
-  })
+    console.log(msg)
+
+    return navigateTo('/')
+  }
 })
