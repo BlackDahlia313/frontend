70f9ee3126a16d27f85bb709e887e0dea94225ea
ruffi0 <matthew@motorcity.host>
2023-06-14 00:53:36 -0400

    test 5324

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,15 @@
 import { useAuth } from '~/store/auth'
-import { onMounted } from 'vue'
 
 const msg = 'You must be logged in to view this page.'
 
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware(({ redirect }) => {
   const user = useAuth()
 
-  onMounted(() => {
-    if (!user.isLoggedIn) {
-      if (process.client) {
-        window.alert(msg)
-      }
-      console.log(msg)
-      return navigateTo('/')
+  if (!user.isLoggedIn) {
+    if (process.client) {
+      window.alert(msg)
     }
-  })
+    console.log(msg)
+    return navigateTo('/')
+  }
 })
