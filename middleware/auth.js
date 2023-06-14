235ac6817339e1920bc0eca29a24604e7bbc89d8
ruffi0 <matthew@motorcity.host>
2023-06-14 01:19:19 -0400

    234

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,31 @@ import { useAuth } from '~/store/auth'
 
 const msg = 'You must be logged in to view this page. Redirecting back to the home page.'
 
-export default function ({ store, redirect, from }) {
-  const user = useAuth()
+export default function ({ redirect }) {
+  return {
+    async beforeRouteEnter(to, from) {
+      const user = useAuth()
 
-  // Check if the user is logged in
-  if (!user.isLoggedIn) {
-    // Check if the previous route is not the current route
-    // This check is to avoid redirection when accessing the page directly via the URL
-    if (from?.path !== null && from?.path !== '/') {
-      // Redirect to the login page or any other appropriate page
-      return navigateTo('/')
+      if (!user.isLoggedIn) {
+        if (from?.path !== null && from?.path !== '/') {
+          if (window.confirm(msg)) {
+            redirect('/')
+          } else {
+            redirect(from.path)
+          }
+        }
+      }
+    },
+    setup() {
+      const user = useAuth()
+
+      onMounted(() => {
+        if (!user.isLoggedIn) {
+          if (window.confirm(msg)) {
+            redirect('/')
+          }
+        }
+      })
     }
   }
 }
